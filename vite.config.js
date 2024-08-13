a6b6d0bd882568f34dc43525f6131dbf7fb802ea
十载北林 <1143524493@qq.com>
2024-08-13 22:37:24 +0800

    20240813

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,8 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/js/tinymce/tinymce.min.js",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            tinymce: "tinymce/tinymce.min.js",
-        },
-    },
 });

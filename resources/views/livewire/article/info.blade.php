34e837de06754b0313578affbaafcb33d510abed
十载北林 <1143524493@qq.com>
2024-08-14 13:10:40 +0800

    update article style

--- resources/views/livewire/article/info.blade.php
+++ resources/views/livewire/article/info.blade.php
@@ -11,8 +11,8 @@
     </div>
 
     <div class="container mx-auto" style="margin-top: 12px;">
-        <div class="p-10 bg-white shadow-xl shadow-indigo-300/10">
-            <div class="text-[#93a2ce] prose">
+        <div class="p-10 bg-white shadow-xl shadow-indigo-300/10 w-auto">
+            <div class="text-[#93a2ce] prose md:prose-lg lg:prose-xl prose-headings:text-indigo-600 prose-code:text-pink-500 prose-code:bg-gray-100 prose-code:p-1 prose-code:rounded-md prose-code:border prose-code:border-gray-200 prose-pre:p-0 prose-pre:bg-gray-100 prose-pre:overflow-x-auto prose-pre:rounded-md prose-pre:border prose-pre:border-gray-200 prose-pre:shadow-none prose-pre:text-sm">
                 {!! $article->content !!}
             </div>
         </div>

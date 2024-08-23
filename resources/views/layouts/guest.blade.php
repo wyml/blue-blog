0c9251738d488d9baa06e08139415ba0c2777856
十载北林 <1143524493@qq.com>
2024-08-23 23:33:24 +0700

    update readme.md and footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,10 +25,17 @@
     @livewireStyles
 </head>
 
-<body>
-    <div class="font-sans text-gray-900 antialiased">
+<body class="flex flex-col  min-h-screen">
+    <div class="flex-1 font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
+
+    <footer class="w-full text-sm font-light bg-white p-12 text-center text-gray-400">
+        <div>&copy; 2023 - {{ date('Y') }} <a class="text-indigo-400" href="{{route('home')}}">LandonBlue</a> Power By <a target="_blank" href="https://github.com/wyml/blue-blog">Blue-BLOG</a> </div>
+        <div>
+            <a class="text-indigo-400" href="https://beian.miit.gov.cn/" target="_blank">桂ICP备2020007823号-4</a>
+        </div>
+    </footer>
 
     @livewireScripts
 </body>
